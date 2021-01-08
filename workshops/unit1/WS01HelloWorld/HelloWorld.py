ce448f49fd36ee1485eb1caa42dca546622c5ab2
elascano <edison_lascano@yahoo.com>
2021-01-08 13:24:42 -0500

    WS01

---
+++ workshops/unit1/WS01HelloWorld/HelloWorld.py
@@ -0,0 +1,14 @@
+print("Hello World from Edison Lascano \n ESPE \n GEO")
+
+# input variables
+addend1 = input('Enter the first number --> ')
+addend2 = input('Enter the second number--> ')
+
+# add two numbers
+sum = int(addend1) + int(addend2)
+
+# displaying the sum
+print('The sum of {0} and {1} is {2}'.format(addend1, addend2, sum))
+
+print('The sum is %.1f' %(float(input('Enter first number: '))+float(input('Enter second number: '))))
+
