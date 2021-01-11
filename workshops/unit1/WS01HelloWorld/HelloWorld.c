826f596c5b78ba9f5a536f889ddc3ae49c89b8cc
elascano <edison_lascano@yahoo.com>
2021-01-11 14:00:06 -0500

    WS04-LogicalOperators

--- workshops/unit1/WS01HelloWorld/HelloWorld.c
+++ workshops/unit1/WS01HelloWorld/HelloWorld.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 int main(){
-    int grade=20;
+    short int grade=33000;
     //printf() displays information
     printf("Hello World from \t \"  Edison Lascano! \" =) \n");
     printf("\n The grade is %d  \n", grade);
+
+    printf("size of int -->  %d \n", sizeof(int));
+
     return 0;
 }
