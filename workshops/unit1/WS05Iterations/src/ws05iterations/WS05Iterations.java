f01f787bee8a310a720f368a1eac2ac6a3f6c6a2
elascano <edison_lascano@yahoo.com>
2021-01-15 14:06:11 -0500

    WS07 Menu sitch-case

--- workshops/unit1/WS05Iterations/src/ws05iterations/WS05Iterations.java
+++ workshops/unit1/WS05Iterations/src/ws05iterations/WS05Iterations.java
@@ -5,6 +5,8 @@
  */
 package ws05iterations;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Edison Lascano Hypertech ESPE-DCCO
@@ -18,7 +20,12 @@ public class WS05Iterations {
         // TODO code application logic here
 
         int i;
-        i = 0;
+        i = 0; //variable
+        final float pi = 3.14F; //constant
+        System.out.println("pi -> " +pi);
+        float radio=1;
+        float area=pi * radio * radio;
+        
 
         System.out.println("==== This is a while loop from 1 to 10 ====");
 
@@ -37,21 +44,67 @@ public class WS05Iterations {
         }
 
         System.out.println("First 5 odd numbers");
-        
+
         int stop;
-        stop=20;
-        for(int j=1 ; j < stop ; j+=2){
+        stop = 20;
+        for (int j = 1; j < stop; j += 2) {
             System.out.println("odd number -> " + j);
         }
-        
-        System.out.println("Multiplication Tables : 9");
-        int top=12;
-        int product=0;
-        int table = 9;
-        for (int j = 1 ; j <= top ; j++){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter The table you want to study: ");
+        int table = scanner.nextInt();
+
+        System.out.println("Multiplication Tables : " + table);
+        int top = 12;
+        int product = 0;
+        for (int j = 1; j <= top; j++) {
             product = table * j;
-            System.out.println("7 * " + j + " = " + product);
+            System.out.println("9 * " + j + " = " + product);
         }
+
+        System.out.println("pi -> " + pi);
+        int number = 9;
+        float realNumber = 0.0F;
+        
+        // 7.7 -> float
+        // 7   -> int or float
+
+        realNumber = number; //casting conversión de datos de int a float
+        
+        System.out.println("integer number  --> " + number);
+        System.out.println("float realNumber --> " + realNumber);
+        
+        realNumber = 6.7F;
+        
+        number = (int) realNumber; //casting o coversión de datos de float a int
+        
+        System.out.println("integer number  --> " + number);
+        System.out.println("float realNumber --> " + realNumber);
+        
+        number = (int) Math.round(realNumber);
+
+        System.out.println("integer number  --> " + number);
+        System.out.println("float realNumber --> " + realNumber);
+        
+        
+        /*
+        
+        
+        System.out.println("realNumber " + realNumber);
+        System.out.println("number ->  " + number);
+
+        realNumber = 5.7F;
+        number = (int) realNumber;
+        System.out.println("realNumber " + realNumber);
+        System.out.println("number ->  " + number);
+        
+        realNumber = 5.7F;
+        number = (int) Math.round(realNumber);
+        System.out.println("realNumber " + realNumber);
+        System.out.println("number ->  " + number);
+        
+        */
+
     }
 }
-
---
+++ workshops/unit1/WS07Menus/src/ws07menus/WS07Menus.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws07menus;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Edison Lascano Hypertech ESPE-DCCO
+ */
+public class WS07Menus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        boolean mainLoop = true;
+        int option;
+
+        do {
+            System.out.println(" ========= Calculator =======");
+            System.out.println("1. -> Addition");
+            System.out.println("2. -> Subtraction");
+            System.out.println("3. -> Multiplication");
+            System.out.println("4. -> Division");
+            System.out.println("5. -> Exit");
+
+            System.out.println("Enter your menu option --> ");
+            option = input.nextInt();
+
+            switch (option) {
+
+                case 1:
+                    int addend1;
+                    int addend2;
+                    int sum;
+                    System.out.println("enter addend 1 -> ");
+                    addend1 = input.nextInt();
+                    System.out.println("enter addend 2 -> ");
+                    addend2 = input.nextInt();
+                    sum = addend1 + addend2;
+                    System.out.println(" the sum is --> " + sum);
+                    break;
+                case 2:
+                    System.out.println("The subtraction is");
+                    break;
+                case 3:
+                    System.out.println("The multiplcation is");
+                    break;
+                case 4:
+                    System.out.println("The division is");
+                    break;
+                case 5:
+                    System.out.println("Good Bye my friend");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid option\n\n\n");
+                    break;
+
+            }
+
+        } while (option != 5);
+
+    }
+
+}
